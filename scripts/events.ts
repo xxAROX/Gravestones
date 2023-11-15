99f0302a59772dd8ef3e73deebe64cc146d376f2
xxAROX <arox.email.0@gmail.com>
2023-11-15 13:15:42 +0100

    base

--- scripts/events.ts
+++ scripts/events.ts
@@ -14,13 +14,18 @@ function onDeathAfter(event: EntityDieAfterEvent): void{
 	const location = player.location;
 	player.sendMessage("EntityDieAfterEvent at " + `X=${location.x}; Y=${location.y}; Z=${location.z}`);
 
-	const inventoryComponent = player.getComponent(EntityInventoryComponent.componentId);
-	if (!(inventoryComponent instanceof EntityInventoryComponent)) return;
-
+	// NOTE: Copy the inventory
+	const inv = player.getComponent(EntityInventoryComponent.componentId) as EntityInventoryComponent;
+	const contents = [];
+	for (let slot=0; slot<inv.container.size; slot++) {
+		contents[slot] = inv.container.getItem(slot);
+	}
 	const block = dimension.getBlock(location);
 	if (!block) return;
 	block.setPermutation(BlockPermutation.resolve("gravestones:default_gravestone"));
-	block.getComponent(BlockInventoryComponent.componentId);
+	const entity = dimension.spawnEntity("minecraft:slime", location);
+	const grave_inventory = block.getComponent(BlockInventoryComponent.componentId) as BlockInventoryComponent;
+	player.sendMessage(grave_inventory ? "grave_inventory exists" : "grave_inventory doesn't exists");
 }
 
 export {
