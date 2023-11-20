1d4997dbd137821d4a7c85188f7b7ea403aae236
Jan Sohn <arox.email.0@gmail.com>
2023-11-20 10:53:58 +0000

    patch

--- scripts/main.ts
+++ scripts/main.ts
@@ -2,9 +2,7 @@ import {system, world} from "@minecraft/server";
 import {onDeathAfter} from "./events";
 
 function main() {
-	world.afterEvents.entityDie.subscribe(onDeathAfter, {
-		entityTypes: ["player"]
-	});
+	world.afterEvents.entityDie.subscribe(onDeathAfter);
 }
 
 system.run(main);
